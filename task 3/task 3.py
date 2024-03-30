0461638adaefcbc5d626482faa9cc8401870e3a9
MLess <BearForest2006@gmail.com>
2024-03-30 12:01:38 +0300

    third task solve

---
+++ task 3/task 3.py
@@ -0,0 +1,20 @@
+from common_functions import *
+
+
+input_path = "C:\\Users\\MLess\\PycharmProjects\\mcko\\task 1\\answer_task_2.txt"
+
+data = get_space_ships_data(input_path)
+
+ship_dict = dict()
+for ship in data:
+    ship_dict['-'.join(ship[0])] = ship
+while True:
+    ship_name = input().strip()
+    if ship_name == "stop":
+        break
+    if ship_name not in ship_dict.keys():
+        print("error.. er.. ror..")
+    else:
+        ship = ship_dict[ship_name]
+        direction = " ".join(list(map(str, ship[3])))
+        print(f"Корабль {ship_name} был отправлен с планеты: {ship[1]} и его направление движения было: {direction}")
